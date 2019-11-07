f1b966a74045e938b1564ba823ff525bac2136c7
snal <sebastien.nal@imt-atlantique.net>
2019-11-07 10:30:39 +0100

    new fill for the identification of the name of a giving mmsi of a ship

---
+++ find_name_of_ships.py
@@ -0,0 +1,17 @@
+"""This fill search for the names of ships
+
+To find the corresponding names, only the mmsi is required. The programm will
+automatically find the corresponding names in the database if it exists.
+Otherwise an error will be printed.
+In a future version it might be possible to look for the best matches (i.e.
+assuming there was a small mistake in the mmsi name and looking for the best
+correspondance)
+
+"""
+
+#from __future__ import barry_as_FLUFL
+
+#__all__ = []
+__version__ = 0.1
+__author__ = snal
+
