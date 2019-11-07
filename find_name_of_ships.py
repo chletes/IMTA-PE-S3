46582271346533ed788d0ecfa0b1bff32fbd0bc5
snal <sebastien.nal@imt-atlantique.net>
2019-11-07 11:00:10 +0100

    writing the main part of the file : extract the database and search for the names (first version)

--- find_name_of_ships.py
+++ find_name_of_ships.py
@@ -15,3 +15,41 @@ correspondance)
 __version__ = 0.1
 __author__ = snal
 
+import xlrd
+
+def import_database(path_of_the_database):
+	"""open the database and convert it to a dictionnary
+	return the dictionnary
+	"""
+	workbook = xlrd.open_workbook(path_of_the_database)
+	workbook = xlrd.open_workbook(path_of_the_database, on_demand = True)
+	worksheet = workbook.sheet_by_index(0)
+	first_row = [] # The row where we stock the name of the column
+	for col in range(worksheet.ncols):
+	    first_row.append( worksheet.cell_value(0,col) )
+	# tronsform the workbook to a list of dictionnary
+	data =[]
+	for row in range(1, worksheet.nrows):
+	    elm = {}
+	    for col in range(worksheet.ncols):
+	        elm[first_row[col]]=worksheet.cell_value(row,col)
+	    data.append(elm)
+	return data
+
+def find_name_of_ships(list_of_mmsi):
+	"""find the name of all corresponding ships by using there mmsi
+	return a dictionnary with the mmsi as key and the name
+	A list is also returned which contained all mmsi which aren't registred in
+	the database.
+	"""
+	database_of_ships = import_database('specify the path of the database')
+	# creating the dictionnary and the list returned
+	names_of_the_ships = {}
+	unknown_ships_mmsi = []
+	# looking for the names of the ships
+	for mmsi in list_of_mmsi:
+		try:
+			names_of_the_ships[mmsi] = database_of_ships[mmsi]  # should not work yet
+		except:
+			unknown_ships_mmsi.append(mmsi)  # stocking unknown mmsi
+	return names_of_the_ships,unknown_ships_mmsi
