e5f8b0a2564c9704e242190312ac041aa0d3cb04
chletes <40454278+chletes@users.noreply.github.com>
2019-11-13 19:06:47 +0100

    Ajouté le programme en boucle.

--- main.py
+++ main.py
@@ -29,26 +29,48 @@ def first_function():
 			distance entre deux bateaux maximale, etc.)
 		3. On lance le programme. 
 	"""
-	print("Bienvenu à notre logiciel!")
-	print("")
-	possibles_choix = {	1:"Mettre a jour les types de bateaux. ", 
-						2:"Acceder au fichier de configuration. ",
-						3:"Chercher les possibles transbordements. "}
-	for q, a in possibles_choix.items():
-		print('{0}. {1}'.format(q, a))
-	print("")
+	
 	choix = 0;
-	while choix == 0:
+	while 1:
+		print("\nBienvenu à notre logiciel!")
+		print("")
+		possibles_choix = {	0:"Sortir du programme. ",
+							1:"Mettre a jour les types de bateaux. ", 
+							2:"Acceder au fichier de configuration. ",
+							3:"Chercher les possibles transbordements. "}
+		for q, a in possibles_choix.items():
+			print('{0}. {1}'.format(q, a))
+		print("")
 		try:
 			choix = int(input("Entrez votre décision: "))
-			if choix not in [1, 2, 3]:
+			if choix not in [0, 1, 2, 3]:
 				print ("Choix incorrect !")
 				choix = 0
 			else:
 				print('Choix fait: {0}'.format(possibles_choix[choix]))
+				if choix == 0:
+					break
+				if choix == 1:
+					#Mise a jour des types de bateaux
+					print("choix 1")
+				if choix == 2:
+					#Accede au fichier de configuration
+					lecture_fichier_configuration()
+					#print("choix 2")
+				if choix == 3:
+					#Chercher les possibles transbordements
+					mensajes123 = [];
+					mensajes5 = [];
+					path = parametres['GENERAL'][0]['PATH']
+					#for filename in glob.glob(os.path.join(path, '*.txt')):# pour lire tous les fichiers qui finisent par .txt
+					for filename in os.listdir(path):
+						#print(filename)
+						real_filename = path + '/' + filename
+						n_mensajes123, n_mensajes5, n_lineas_malas = decode(real_filename, mensajes123, mensajes5)
+					print('Les fichiers avaient {0} messages de type 1, 2, ou 3 , {1} messages de type 5 et {2} messages undécodables.'.format(n_mensajes123, n_mensajes5, n_lineas_malas))
 		except ValueError :
 			print("Choix incorrect ! Saisisez un numero aussi!")
-	return choix
+	 
 ##############################################################################
 #tests
 ##############################################################################
@@ -56,23 +78,6 @@ parametres ={}
 parametres=get_parameters()
 
 if parametres != {}:
-	choix = first_function()
-	if choix == 1:
-		#Mise a jour des types de bateaux
-		print("choix 1")
-	if choix == 2:
-		#Accede au fichier de configuration
-		lecture_fichier_configuration()
-		#print("choix 2")
-	if choix == 3:
-		#Chercher les possibles transbordements
-		mensajes123 = [];
-		mensajes5 = [];
-		path = parametres['GENERAL'][0]['PATH']
-		#for filename in glob.glob(os.path.join(path, '*.txt')):# pour lire tous les fichiers qui finisent par .txt
-		for filename in os.listdir(path):
-			#print(filename)
-			real_filename = path + '/' + filename
-			n_mensajes123, n_mensajes5, n_lineas_malas = decode(real_filename, mensajes123, mensajes5)
-		print('Les fichiers avaient {0} messages de type 1, 2, ou 3 , {1} messages de type 5 et {2} messages undécodables.'.format(n_mensajes123, n_mensajes5, n_lineas_malas))
+	first_function()
+	
 		
